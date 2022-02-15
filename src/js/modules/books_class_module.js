6f2939b862ccfaaff226647b97c2f0dcdebaf4ab
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-15 10:25:57 +0200

    Add books class with add get and remove functions

---
+++ src/js/modules/books_class_module.js
@@ -0,0 +1,21 @@
+import Book from './book_class_module';
+
+export default class Books {
+  constructor() {
+    this.books = [];
+  }
+
+  addBooks(title, author, id) {
+    const b = new Book(title, author, id); 
+    this.books.push(b); 
+  }
+
+  getBooks() {
+    return this.books;
+  }
+
+  removeBook(id) {
+    const books = this.books.filter((b) => b.id.toString() !== id);
+    this.books = books;
+  }
+}
