e766e89f481b38d95cebc0518b2b5c1e33263d4c
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-15 10:48:38 +0200

    Update luxon importing directory + Add class functionality to index.js

--- src/js/modules/luxon.js
+++ src/js/modules/luxon.js
@@ -1,4 +1,8 @@
-import { DateTime } from "luxon";
+// import { DateTime } from "luxon";
+import { DateTime } from '../../../node_modules/luxon/build/es6/luxon';
+
 
 const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
 export default now;
+
+
